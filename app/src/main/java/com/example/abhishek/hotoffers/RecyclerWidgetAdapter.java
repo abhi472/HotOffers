00969edc3980949e13f552d867b131e0497ac626
abhishek <abhitiwari472@gmail.com>
2016-04-18 17:21:10 +0530

    added a basic widget with listview

---
+++ app/src/main/java/com/example/abhishek/hotoffers/RecyclerWidgetAdapter.java
@@ -0,0 +1,89 @@
+package com.example.abhishek.hotoffers;
+
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService.RemoteViewsFactory;
+
+import java.util.ArrayList;
+
+public class RecyclerWidgetAdapter implements RemoteViewsFactory {
+    private ArrayList<Content> content = new ArrayList<>();
+    Content contents = new Content();
+    private Context context = null;
+    private int appWidgetId;
+
+    public RecyclerWidgetAdapter(Context context, Intent intent) {
+        this.context = context;
+        appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+
+        populateListItem();
+    }
+
+    private void populateListItem() {
+
+        for (int i = 0; i < 100; i++) {
+            contents.desc = "Best Collections of Bike Accessories Helmets and Spares starting from 99 only";
+            content.add(contents);
+
+        }
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return content.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    /*
+    *Similar to getView of Adapter where instead of View
+    *we return RemoteViews
+    *
+    */
+    @Override
+    public RemoteViews getViewAt(int position) {
+        final RemoteViews remoteView = new RemoteViews(
+                context.getPackageName(), R.layout.widget_card);
+
+        remoteView.setTextViewText(R.id.textView,content.get(position).desc );
+
+        return remoteView;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+}
